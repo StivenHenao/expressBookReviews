8c9e10819f2db3e9da19690fcb0a34aed1c25098
Stiven Henao <142904664+StivenHenao@users.noreply.github.com>
2025-04-06 12:43:33 -0500

    feat: get book by review and register user

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,8 +6,22 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: "Username and password are required." });
+  } else {
+    // Check if the username already exists
+    const userExists = users.some(user => user.username === username);
+    if (userExists) {
+      return res.status(400).json({ message: "Username already exists." });
+    } else {
+      // Add the new user to the users array
+      users.push({ username, password });
+      return res.status(200).json({ message: "User registered successfully." });
+    }
+  }
 });
 
 // Get the book list available in the shop
@@ -37,8 +51,13 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+  if (book && book.reviews) {
+    res.send(JSON.stringify(book.reviews, null, 4));
+  } else {
+    res.status(404).send("No reviews found for this book.");
+  }
 });
 
 module.exports.general = public_users;
