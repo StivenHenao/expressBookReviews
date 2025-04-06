9f04d050ab94ff37236c42e7923759e590e92319
Stiven Henao <142904664+StivenHenao@users.noreply.github.com>
2025-04-06 12:36:02 -0500

    feat: get book by author and by title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,14 +23,16 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const booksByAuthor = Object.values(books).filter(book => book.author === author);
+  res.send(JSON.stringify(booksByAuthor, null, 4));
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  const booksByTitle = Object.values(books).filter(book => book.title === title);
+  res.send(JSON.stringify(booksByTitle, null, 4));
 });
 
 //  Get book review
